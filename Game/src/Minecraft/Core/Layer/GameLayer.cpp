df9755f69cbb0c09f09ae721f7f0d9a15f89ad18
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-10 21:46:05 +0200

    Update GameLayer

--- Game/src/Minecraft/Core/Layer/GameLayer.cpp
+++ Game/src/Minecraft/Core/Layer/GameLayer.cpp
@@ -1,35 +1,50 @@
 #include "mcpch.h"
 #include "Minecraft/Core/Layer/GameLayer.h"
 
+#include "Minecraft/Renderer/Renderer.h"
 
 namespace Minecraft {
 
 	GameLayer::GameLayer()
 		: Layer("GameLayer")
 	{
-
 	}
 
 	void GameLayer::OnAttach()
 	{
-	}
+		Renderer::Init();
 
-	void GameLayer::OnDetach()
-	{
+		Application::Get().GetWindow().DisableCursor();
+
+		m_Camera = CreateRef<GameCamera>();
+
+		float windowWidth = Application::Get().GetWindow().GetWidth();
+		float windowHeight = Application::Get().GetWindow().GetHeight();
+		m_Camera->SetViewportSize(windowWidth, windowHeight);
+
+		uint64_t seed = Random::UInt();
+		m_World = CreateRef<World>(seed);
 	}
 
 	void GameLayer::OnUpdate(Timestep ts)
 	{
-	}
+		// ---- Update ----
 
-	void GameLayer::OnImGuiRender()
-	{
+		m_Camera->OnUpdate(ts);
+
+		// ---- Render ----
+
+		RenderCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
+		RenderCommand::Clear();
+
+		Renderer::RenderWorld(m_World, m_Camera->GetViewProjection());
 	}
 
 	// -- Event Handling --
 
 	void GameLayer::OnEvent(Event& e)
 	{
+		m_Camera->OnEvent(e);
 	}
 
 }
--- Game/src/Minecraft/Core/Layer/GameLayer.h
+++ Game/src/Minecraft/Core/Layer/GameLayer.h
@@ -2,6 +2,9 @@
 
 using namespace Moon;
 
+#include "Minecraft/Camera/GameCamera.h"
+#include "Minecraft/World/World.h"
+
 
 namespace Minecraft {
 
@@ -12,10 +15,14 @@ namespace Minecraft {
 		virtual ~GameLayer() = default;
 
 		virtual void OnAttach() override;
-		virtual void OnDetach() override;
+		virtual void OnDetach() override {};
 		virtual void OnUpdate(Timestep ts) override;
-		virtual void OnImGuiRender() override;
+		virtual void OnImGuiRender() override {};
 		virtual void OnEvent(Event& e) override;
+
+	private:
+		Ref<World> m_World = nullptr;
+		Ref<GameCamera> m_Camera;
 
 	};
 
