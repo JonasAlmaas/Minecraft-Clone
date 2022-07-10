2b030a4398e72d308530338154fe106cc69f12f0
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-10 21:45:38 +0200

    Create GameCamera

---
+++ Game/src/Minecraft/Camera/GameCamera.cpp
@@ -0,0 +1,131 @@
+#include "mcpch.h"
+#include "Minecraft/Camera/GameCamera.h"
+
+
+namespace Moon {
+
+	GameCamera::GameCamera(float fov, float aspectRatio, float nearClip, float farClip)
+		: m_FOV(fov), m_AspectRatio(aspectRatio), m_NearClip(nearClip), m_FarClip(farClip)
+	{
+		m_LastMouseX = Input::GetMouseX();
+		m_LastMouseY = Input::GetMouseY();
+
+		UpdateView();
+	}
+
+	void GameCamera::OnUpdate(Timestep ts)
+	{
+		// Move Forward
+		if (Input::IsKeyPressed(Key::W))
+		{
+			m_Position.x += sin(m_Yaw) * m_MoveSpeed * ts;
+			m_Position.y += cos(m_Yaw) * m_MoveSpeed * ts;
+		}
+
+		// Move Back
+		if (Input::IsKeyPressed(Key::S))
+		{
+			m_Position.x -= sin(m_Yaw) * m_MoveSpeed * ts;
+			m_Position.y -= cos(m_Yaw) * m_MoveSpeed * ts;
+		}
+
+		// Move Left
+		if (Input::IsKeyPressed(Key::A))
+		{
+			m_Position.x -= cos(m_Yaw) * m_MoveSpeed * ts;
+			m_Position.y += sin(m_Yaw) * m_MoveSpeed * ts;
+		}
+
+		// Move Right
+		if (Input::IsKeyPressed(Key::D))
+		{
+			m_Position.x += cos(m_Yaw) * m_MoveSpeed * ts;
+			m_Position.y -= sin(m_Yaw) * m_MoveSpeed * ts;
+		}
+
+		// Move Up
+		if (Input::IsKeyPressed(Key::Space))
+		{
+			m_Position.z += m_MoveSpeed * ts;
+		}
+
+		// Move Down
+		if (Input::IsKeyPressed(Key::LeftControl))
+		{
+			m_Position.z -= m_MoveSpeed * ts;
+		}
+
+		UpdateView();
+	}
+
+	void GameCamera::SetViewportSize(float width, float height)
+	{
+		m_ViewportWidth = width;
+		m_ViewportHeight = height;
+		UpdateProjection();
+	}
+
+	glm::vec3 GameCamera::GetUpDirection() const
+	{
+		return glm::rotate(GetOrientation(), glm::vec3(0.0f, 1.0f, 0.0f));
+	}
+
+	glm::vec3 GameCamera::GetRightDirection() const
+	{
+		return glm::rotate(GetOrientation(), glm::vec3(1.0f, 0.0f, 0.0f));
+	}
+
+	glm::vec3 GameCamera::GetForwardDirection() const
+	{
+		return glm::rotate(GetOrientation(), glm::vec3(0.0f, 0.0f, -1.0f));
+	}
+
+	glm::quat GameCamera::GetOrientation() const
+	{
+		return glm::quat(glm::vec3(-m_Pitch, 0.0f, -m_Yaw));
+	}
+
+	void GameCamera::UpdateProjection()
+	{
+		m_AspectRatio = m_ViewportWidth / m_ViewportHeight;
+		m_Projection = glm::perspective(glm::radians(m_FOV), m_AspectRatio, m_NearClip, m_FarClip);
+	}
+
+	void GameCamera::UpdateView()
+	{
+		glm::quat orientation = GetOrientation();
+		m_ViewMatrix = glm::translate(glm::mat4(1.0f), m_Position) * glm::toMat4(orientation);
+		m_ViewMatrix = glm::inverse(m_ViewMatrix);
+	}
+
+	// -- Event Handling --
+
+	void GameCamera::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<MouseMovedEvent>(ME_BIND_EVENT_FN(GameCamera::OnMouseMovedEvent));
+		dispatcher.Dispatch<WindowResizeEvent>(ME_BIND_EVENT_FN(GameCamera::OnWindowResizeEvent));
+	}
+
+	bool GameCamera::OnMouseMovedEvent(MouseMovedEvent& e)
+	{
+		float offsetX = e.GetX() - m_LastMouseX;
+		float offsetY = e.GetY() - m_LastMouseY;
+
+		m_LastMouseX = e.GetX();
+		m_LastMouseY = e.GetY();
+
+		m_Pitch += m_Sensitivity * offsetY;
+		m_Yaw += m_Sensitivity * offsetX;
+
+		return false;
+	}
+
+	bool GameCamera::OnWindowResizeEvent(WindowResizeEvent& e)
+	{
+		m_ViewportWidth = e.GetWidth();
+		m_ViewportHeight = e.GetHeight();
+		return false;
+	}
+
+}
---
+++ Game/src/Minecraft/Camera/GameCamera.h
@@ -0,0 +1,67 @@
+#pragma once
+
+using namespace Moon;
+
+#include <Moon/Core/Renderer/Camera/Camera.h>
+
+
+namespace Moon {
+
+	class GameCamera : public Camera
+	{
+	public:
+		GameCamera() = default;
+		GameCamera(float fov, float aspectRatio, float nearClip, float farClip);
+
+		~GameCamera() = default;
+
+		void OnUpdate(Timestep ts);
+		void OnEvent(Event& e);
+
+		void SetViewportSize(float width, float height);
+
+		const glm::mat4& GetViewMatrix() const { return m_ViewMatrix; }
+		glm::mat4 GetViewProjection() const { return m_Projection * m_ViewMatrix; }
+
+		glm::vec3 GetUpDirection() const;
+		glm::vec3 GetRightDirection() const;
+		glm::vec3 GetForwardDirection() const;
+		glm::quat GetOrientation() const;
+		const glm::vec3& GetPosition() const { return m_Position; }
+
+		float GetPitch() const { return glm::degrees(m_Pitch); }
+		float GetYaw() const { return glm::degrees(m_Yaw); }
+		float GetRoll() const { return glm::degrees(m_Roll); }
+
+	private:
+		void UpdateProjection();
+		void UpdateView();
+
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
+	private:
+		float m_MoveSpeed = 4.0f;
+		float m_Sensitivity = 0.001f;
+
+		float m_LastMouseX = 0.0f;
+		float m_LastMouseY = 0.0f;
+
+		float m_FOV = 45.0f;
+		float m_AspectRatio = 1.778f;
+		float m_NearClip = 0.1f;
+		float m_FarClip = 10000.0f;
+
+		glm::mat4 m_ViewMatrix;
+		glm::vec3 m_Position = { 0.0f, 0.0f, 5.0f };
+
+		float m_Pitch = glm::radians(-90.0f);
+		float m_Yaw = 0.0f;
+		float m_Roll = 0.0f;
+
+		float m_ViewportWidth = 1920.0f;
+		float m_ViewportHeight = 1080.0f;
+
+	};
+
+}
