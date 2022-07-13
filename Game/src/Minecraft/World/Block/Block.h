fb1f55d9d16c317e4abe69b84eda05ac7af7dbe5
Almaas <jonasalmaas7@gmail.com>
2022-07-13 02:37:37 +0200

    Update Block

--- Game/src/Minecraft/World/Block/Block.h
+++ Game/src/Minecraft/World/Block/Block.h
@@ -1,56 +1,86 @@
 #pragma once
 
+using namespace Moon;
 
-namespace Minecraft::Block {
 
-	enum class Type
+namespace Minecraft {
+
+	class Block
 	{
-		None = 0,
-		Air,
-		Dirt,
-		Grass,
-		Stone,
-	};
-
-	enum class Texture
-	{
-		GrassTop = 0,
-	};
-
-	struct Vertex
-	{
-		uint32_t LocalPosition;
-		uint32_t TextureIndex;
-		uint32_t RGBI;
-	};
-
-	struct Position
-	{
-		uint8_t X, Y, Z;
-
-		Position(uint8_t x, uint8_t y, uint8_t z)
-			: X(x), Y(y), Z(z) {}
-
-		bool operator == (const Position& other) const
+	public:
+		enum class Type
 		{
-			return X == other.X && Y == other.Y && Z == other.Z;
-		}
-	};
+			Air = 0,
+			Bedrock,
+			Cobblestone,
+			CraftingTable,
+			Dirt,
+			Glass,
+			GlassPaneTop,
+			Grass,
+			Gravel,
+			OakLog,
+			OakPlank,
+			Sand,
+			Stone,
+			Tourch,
+		};
 
-}
-
-namespace std {
-
-	// X and Y can just be in the range of 0 - 15 (4 bit uint)
-	// Z is in the range 0 - 255 (8 bit uint)
-	// This means that I can hash the block position buy putting them all in a uint16
-	template<>
-	struct hash<Minecraft::Block::Position>
-	{
-		size_t operator()(const Minecraft::Block::Position& key) const
+		enum class TextureIndex
 		{
-			return hash<uint16_t>()(key.X + (key.Y << 4) + (key.Z << 8));
-		}
+			GrassTop = 0,
+			GrassSide,
+			Dirt,
+			Cobblestone,
+			Gravel,
+			Sand,
+			Bedrock,
+			OakLogSide,
+			OakLogTop,
+			OakPlank,
+			CraftingTableSide1,
+			CraftingTableSide2,
+			CraftingTableTop,
+			RedstoneLampOff,
+			RedstoneLampOn,
+			Tourch,
+			Stone,
+			CopperOre,
+			DiamondOre,
+			EmeraldOre,
+			IronOre,
+			GoldOre,
+			LapisOre,
+			RedstoneOre,
+			Glass,
+			GlassSide,
+		};
+
+		enum class Face
+		{
+			Top,
+			Bottom,
+			North,
+			South,
+			East,
+			West,
+		};
+
+		enum class Renderpass
+		{
+			Solid = 0,
+			Transparant,
+			Water,
+		};
+
+	public:
+		virtual ~Block() = default;
+
+		static Ref<Block> Create(Type blockType);
+
+		virtual uint16_t GetTextureIndex(Face face) const = 0;
+		virtual Renderpass GetRenderpass() const = 0;
+
 	};
 
 }
