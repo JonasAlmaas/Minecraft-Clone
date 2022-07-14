ffb8547c7b0693043e4d521e53131a4245b4714d
Almaas <jonasalmaas7@gmail.com>
2022-07-14 18:24:45 +0200

    Improve core types

--- Game/src/Minecraft/Core/Types.h
+++ Game/src/Minecraft/Core/Types.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <xhash>
-
 
 namespace Minecraft {
 
@@ -9,50 +7,34 @@ namespace Minecraft {
 	{
 		int X, Y;
 
-		Int2(int x, int y)
-			: X(x), Y(y) {}
+		Int2() : X(0), Y(0) {}
+		Int2(int x, int y) : X(x), Y(y) {}
 
-		bool operator == (const Int2& other) const
-		{
-			return X == other.X && Y == other.Y;
-		}
+		bool operator == (const Int2& other) const { return X == other.X && Y == other.Y; }
+		bool operator != (const Int2& other) const { return !(*this == other); }
+
+		Int2 operator + (const Int2& other) const { return { X + other.X, Y + other.Y }; }
+		Int2 operator - (const Int2& other) const { return { X - other.X, Y - other.Y }; }
+
+		Int2 operator + (int value) const { return { X + value, Y + value }; }
+		Int2 operator - (int value) const { return { X - value, Y - value }; }
 	};
 
 	struct Int3
 	{
 		int X, Y, Z;
 
-		Int3(int x, int y, int z)
-			: X(x), Y(y), Z(z) {}
+		Int3() : X(0), Y(0), Z(0) {}
+		Int3(int x, int y, int z) : X(x), Y(y), Z(z) {}
 
-		bool operator == (const Int3& other) const
-		{
-			return X == other.X && Y == other.Y && Z == other.Z;
-		}
-	};
+		bool operator == (const Int3& other) const { return X == other.X && Y == other.Y && Z == other.Z; }
+		bool operator != (const Int3& other) const { return !(*this == other); }
 
-}
+		Int3 operator + (const Int3& other) const { return { X + other.X, Y + other.Y, Z + other.Z }; }
+		Int3 operator - (const Int3& other) const { return { X - other.X, Y - other.Y, Z - other.Z }; }
 
-namespace std {
-
-	// TODO: This doesn't really work at all :D
-
-	template<>
-	struct hash<Minecraft::Int2>
-	{
-		size_t operator()(const Minecraft::Int2& key) const
-		{
-			return hash<int>()(key.X);
-		}
-	};
-
-	template<>
-	struct hash<Minecraft::Int3>
-	{
-		size_t operator()(const Minecraft::Int3& key) const
-		{
-			return hash<int>()(key.X);
-		}
+		Int3 operator + (int value) const { return { X + value, Y + value, Z + value }; }
+		Int3 operator - (int value) const { return { X - value, Y - value, Z - value }; }
 	};
 
 }
